afcc7a931c047128f2c97d758cedddebee551df8
Rafhael <rafhael4hailar@gmail.com>
2024-02-14 01:45:02 +0800

    Finish the mobile responsiveness featuressssss with swiper gee est

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, MouseEvent } from "react";
+import { FC, useState, useEffect, useRef } from "react";
 import { faFire, faShieldHalved, faPowerOff, faPersonRunning, faHandsHoldingChild, faWrench, faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
@@ -43,11 +43,95 @@ const Cards: CardItem[] = [
 ];
 
 const cardClass = "flex flex-col gap-2 md:w-[19vw] md:h-[14vw] w-[70vw] h-[45vw] rounded-xl justify-center items-center";
-
 let currCardIdx = 0;
+
+//mouse events properties
+let dragging = false;
+let allowDragging = true;
+let startX: number | null = null;
 
 const Services: FC = () => {
     const [currCard,setCurrCard] = useState(0);
+    const [_,rerender] = useState(false);
+    const swiperContainerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        
+       let container:HTMLDivElement = swiperContainerRef.current as HTMLDivElement;
+       const cards: HTMLCollection = container.children;
+
+       function getTranslateXValue(classes: string): number {
+           const classX = classes.match(/(-)?translate-x-\[(\d*)vw\]/) as Array<string>;
+           let translateX: number | undefined;
+
+           if (classX.length > 2) {
+              translateX = Number(classX[2]) / 100 * window.innerWidth;
+              
+              if (classX[1] == "-") translateX = -translateX;
+           }
+
+           return translateX as number;
+       }
+
+       container.addEventListener("touchstart",function(event: TouchEvent): void {
+           if (!allowDragging) return;
+
+           const x = event.touches[0].clientX;
+           dragging = true;
+           startX = x;
+           console.log(startX);
+       });
+
+       container.addEventListener("touchend",function(): void {
+           dragging = false;
+            
+           for (let i = 0;i < cards.length;i++) {
+              const card = cards[i] as HTMLDivElement;
+
+              card.style.transition = "1s all ease-out"; 
+              let _ = card.style.transition;
+              card.style.transform = "";
+           }
+       });
+
+       container.addEventListener("touchmove",function(event: TouchEvent): void {
+           if (!dragging) return;
+
+           const x = event.touches[0].clientX;
+           const deltaX = x - (startX as number);
+           let updateCurrCard = 0;
+
+           const offsetX = 200;
+            
+           console.log(deltaX);
+           if (deltaX <= -offsetX) updateCurrCard = -1; 
+           else if (deltaX >= offsetX) updateCurrCard = 1;
+
+           if (updateCurrCard !== 0) {
+              setCurrCard(current => {
+                  let next = current + updateCurrCard;
+                  next = next % Cards.length;
+                  return next >= 0 ? next : Cards.length - 1;
+              });
+              startX = null;
+              dragging = false;
+
+              const newEvent = new Event("touchend");
+              this.dispatchEvent(newEvent);
+              allowDragging = false;
+              setTimeout(() => allowDragging = true,1000); // wait one second to drag again.
+              return;
+           }
+        
+           for (let i = 0;i < cards.length;i++) {
+               const card = cards[i] as HTMLDivElement;
+               const translateX = getTranslateXValue(card.className);
+               const scaleX = card.getBoundingClientRect().width / card.offsetWidth;
+               
+               card.style.transform = `translateX(${translateX + deltaX}px) scale(${scaleX}) `;
+           }
+       });
+    },[]);
     
     return <div className="py-[7vw] bg-black md:px-[5vw] flex items-center">
         <div className="flex flex-col gap-[2vw]">
@@ -73,19 +157,22 @@ const Services: FC = () => {
                      })
                 }
             </div>
-            <div className="flex md:hidden h-[45vw] w-screen overflow-hidden ">
+            <div ref={swiperContainerRef} className="flex md:hidden h-[45vw] w-screen relative overflow-hidden ">
                 {
                    Cards.map((card: CardItem,index: number) => {
                       const {icon,title,semiTitle} = card; 
-                      let translateX = "translate-x-[100vw]";
+                      let translateX = "translate-x-[155vw]";
                       let scale = "scale-[0.9]";
                       let opacity = "opacity-40";
 
-                      let currCardRight = (index + 1) % Cards.length;
+                      const currCardRight = (index + 1) % Cards.length;
+                      const currCardResRight = (index + 2) % Cards.length;
                       let currCardLeft = index - 1;
+                      let isNotDisplayed = false;
                       
                       if (index == 0) currCardLeft = Cards.length - 1;
-                      
+
+                      const currCardResLeft = currCardLeft > 0 ? currCardLeft - 1 : 0;
 
                       switch (currCard) {
                          case index:
@@ -99,9 +186,17 @@ const Services: FC = () => {
                          case currCardLeft:
                             translateX = "-translate-x-[55vw]"
                             break;
+                         case currCardResLeft:
+                            translateX = "-translate-x-[125vw]";
+                            break;
+                         case currCardResRight: 
+                            translateX = "translate-x-[155vw]";
+                            break;
+                         default:
+                            isNotDisplayed = true;
                       } 
 
-                      return <div className={`${cardClass} bg-white absolute ${translateX} ${scale} ${opacity}`}>
+                      return <div data-not-display={isNotDisplayed} className={`${cardClass} bg-white absolute ${translateX} ${scale} ${opacity} ${isNotDisplayed ? "hidden" : ""}`}>
                                 <FontAwesomeIcon icon={icon} className="md:text-[5vw] text-[13vw]"/>
                                 <div className="text-center ">
                                     <div className="font-bold md:text-[1.3vw] text-[6vw] ">{title}</div>
--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -2,27 +2,58 @@ import {FC} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDocker, faDailymotion, faApple, faReact, faPaypal, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+
 const Sponsors: FC = () => {
-    return <div className="py-[2vw] bg-black text-blue flex md:text-[7vw] text-[26vw] overflow-hidden justify-center md:gap-[6vw] gap-[14vw]">
-        <div>
-            <FontAwesomeIcon icon={faDocker} />
+    return <>
+         <div className="md:hidden flex">
+             <Swiper autoplay={{delay: 2000,disableOnInteraction: false}} modules={[Autoplay]} slidesPerView={3} spaceBetween={20} className="text-center py-[2vw] bg-black text-blue md:text-[7vw] text-[26vw] overflow-hidden md:gap-[6vw] gap-[14vw]">
+                <SwiperSlide>
+                    <FontAwesomeIcon icon={faDocker} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <FontAwesomeIcon icon={faDailymotion} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <FontAwesomeIcon icon={faApple} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <FontAwesomeIcon icon={faReact} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <FontAwesomeIcon icon={faPaypal} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <FontAwesomeIcon icon={faXTwitter} />
+                </SwiperSlide>
+            </Swiper>
         </div>
-        <div>
-            <FontAwesomeIcon icon={faDailymotion} />
+        <div className="py-[2vw] bg-black text-blue md:flex hidden md:text-[7vw] text-[26vw] overflow-hidden justify-center md:gap-[6vw] gap-[14vw]">
+            <div>
+                <FontAwesomeIcon icon={faDocker} />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faDailymotion} />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faApple} />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faReact} />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faPaypal} />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faXTwitter} />
+            </div>
         </div>
-        <div>
-            <FontAwesomeIcon icon={faApple} />
-        </div>
-        <div>
-            <FontAwesomeIcon icon={faReact} />
-        </div>
-        <div>
-            <FontAwesomeIcon icon={faPaypal} />
-        </div>
-        <div>
-            <FontAwesomeIcon icon={faXTwitter} />
-        </div>
-    </div>
+    </>
 }
 
 export default Sponsors;
